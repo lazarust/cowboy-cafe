018bbe0e7ef1df2a86a07c2e329e9fc58c955fc4
lazarust <tlazarus@ksu.edu>
2020-01-22 16:55:58 -0600

    Move classes to their own files

---
+++ Data/PecosPulledPork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Pecos Pulled Pork entree
+    /// </summary>
+    public class PecosPulledPork
+    {
+        /// <summary>
+        /// The price of the pork
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.88;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the pork
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 528;
+            }
+        }
+
+        private bool bread = true;
+        /// <summary>
+        /// If the pork has bread
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the pork has pickles
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// Any special instructions for the pork
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
