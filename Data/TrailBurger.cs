cc31c4a21476ee34dc71ffbc82b5399f8085b5ac
lazarust <tlazarus@ksu.edu>
2020-01-22 16:55:58 -0600

    Move classes to their own files

---
+++ Data/TrailBurger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Trail Burger entree
+    /// </summary>
+    public class TrailBurger
+    {
+        /// <summary>
+        /// The price of the burger
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 4.5;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the burger
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 288;
+            }
+        }
+
+        private bool ketchup = true;
+        /// <summary>
+        /// If the burger has ketchup
+        /// </summary>
+        public bool Ketchup
+        {
+            get { return ketchup; }
+            set { ketchup = value; }
+        }
+
+        private bool mustard = true;
+        /// <summary>
+        /// If the burger has mustard
+        /// </summary>
+        public bool Mustard
+        {
+            get { return mustard; }
+            set { mustard = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the burger has pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        private bool cheese = true;
+        /// <summary>
+        /// If the burger has cheese
+        /// </summary>
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        /// <summary>
+        /// Special instructions for the burger
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!ketchup) instructions.Add("hold ketchup");
+                if (!mustard) instructions.Add("hold mustard");
+                if (!pickle) instructions.Add("hold pickle");
+                if (!cheese) instructions.Add("hold cheese");
+
+                return instructions;
+            }
+        }
+    }
+}
