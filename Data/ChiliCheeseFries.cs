0593465b6599b5fae59d267d492a1e6b7d5c4b1b
lazarust <tlazarus@ksu.edu>
2020-02-03 16:46:18 -0600

    ChilliCheeseFries

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Class to hand chili cheese fries side
+    /// </summary>
+    public class ChiliCheeseFries: Side
+    {
+        /// <summary>
+        /// Calories for the side based off of size
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 610;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Small:
+                        return 433;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Price for the side based off of size
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 3.99;
+                    case Size.Medium:
+                        return 2.99;
+                    case Size.Small:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+    }
+}
