fbdb033be4874fc12eb919860f59df2976328443
lazarust <tlazarus@ksu.edu>
2020-02-03 16:51:56 -0600

    CornDodgers Side Class

---
+++ Data/CornDodgers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class CornDodgers: Side
+    {
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 717;
+                    case Size.Medium:
+                        return 685;
+                    case Size.Small:
+                        return 512;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 1.99;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Small:
+                        return 1.59;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+    }
+}
