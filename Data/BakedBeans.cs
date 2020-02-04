12019626c9d2148cf14f3fc8d51d1c1c3fc3a4b7
lazarust <tlazarus@ksu.edu>
2020-02-04 16:17:09 -0600

    BakedBeans

---
+++ Data/BakedBeans.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Author: Thomas Lazarus
+ * Class: BakedBeans
+ * Purpose: Handle the baked beans side
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Class for the baked beans side
+    /// </summary>
+    public class BakedBeans: Side
+    {
+        /// <summary>
+        /// Calories of the side
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 410;
+                    case Size.Medium:
+                        return 378;
+                    case Size.Small:
+                        return 312;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Price of the side 
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 1.99;
+                    case Size.Medium:
+                        return 1.79;
+                    case Size.Small:
+                        return 1.59;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+    }
+}
