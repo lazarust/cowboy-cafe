5273e304024bd1ee1d4ef6128f339b012d08ff3a
lazarust <tlazarus@ksu.edu>
2020-02-11 15:56:06 -0600

    JerkedSoda

---
+++ Data/JerkedSoda.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Author: Thomas Lazarus
+ * Class: JerkedSoda
+ * Purpose: Handle the jerked soda drink
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Jerked soda drink class
+    /// </summary>
+    public class JerkedSoda: Drink
+    {
+        /// <summary>
+        /// Price of the drink
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 2.59;
+                    case Size.Medium:
+                        return 2.10;
+                    case Size.Small:
+                        return 1.59;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calories of the drink
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 198;
+                    case Size.Medium:
+                        return 146;
+                    case Size.Small:
+                        return 110;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Flavor of the drink
+        /// </summary>
+        public SodaFlavor Flavor;
+
+        /// <summary>
+        /// Special Instructions for the drink
+        /// </summary>
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!Ice) instructions.Add("Hold Ice");
+
+                return instructions;
+            }
+        }
+    }
+}
---
+++ Data/SodaFlavor.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author: Thomas Lazarus
+ * Class: SodaFlavor
+ * Purpose: Handle all the soda flavors
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public enum SodaFlavor
+    {
+        CreamSoda,
+        OrangeSoda,
+        Sarsparilla,
+        BirchBeer,
+        RootBeer
+    }
+}
