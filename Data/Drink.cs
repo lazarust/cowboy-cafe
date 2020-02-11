967af68d2bdec925e2bc6818e26a11a767c159e9
lazarust <tlazarus@ksu.edu>
2020-02-11 15:56:06 -0600

    JerkedSoda

---
+++ Data/Drink.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Author: Thomas Lazarus
+ * Class: Drink
+ * Purpose: Handle all the drink classes
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        /// <summary>
+        /// Price of the drink
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// Calories of the drink
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// Gets the size of the side
+        /// </summary>
+        public virtual Size Size { get; set; }
+
+        /// <summary>
+        /// Special instructions for the drink
+        /// </summary>
+        public abstract List<string> SpecialInstructions { get; }
+
+        private bool ice = true;
+        /// <summary>
+        /// If the drink has ice defaults to true   
+        /// </summary>
+        public virtual bool Ice
+        {
+            get { return ice; }
+            set { ice = value; }
+        }
+    }
+}
